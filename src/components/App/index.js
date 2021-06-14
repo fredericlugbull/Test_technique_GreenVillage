54a27eba68a9c636561eec0ecca9306e37eef91b
fredericlugbull <f.lugbull@gmail.com>
2021-06-14 17:58:58 +0200

    Made listing prettier

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -12,23 +12,24 @@ import './styles.scss';
 const App = () => {
 // Here I parse the array of strings and construct Objects instead, with keys and values that were asked.
   const dataParsed = data.map(eachString => { return {T: eachString.charAt(6), PLU: parseInt( eachString.substring(12, 16)), CM: eachString.substring(eachString.lastIndexOf("M:") + 2, eachString.lastIndexOf("_VA")), VA: eachString.substring(eachString.lastIndexOf("VA:") + 3, eachString.lastIndexOf("_C:")) };});
-  console.log("my parsed Data is:", dataParsed);
 
   return (
     <div className="app">
       <h1>Liste des produits</h1>
-    {/* Display of every product cards */}
    
+   <div className="productList">
 {/* Displaying of the products*/}
-    {dataParsed.map((productCard, i) => {
-    return (
-      <ul key={i}>
-        <li>Type: {productCard.T}</li>
-        <li>Type: {productCard.PLU}</li>
-        <li>Type: {productCard.CM}</li>
-        <li>Type: {productCard.VA}</li>
-      </ul>
-    );})}
+{dataParsed.map((productCard, i) => {
+      return (
+        <ul key={i}>
+          <li>Type: {productCard.T}</li>
+          <li>Type: {productCard.PLU}</li>
+          <li>Type: {productCard.CM}</li>
+          <li>Type: {productCard.VA}</li>
+        </ul>
+    )})}
+   </div>
+
   </div>
 );
 };
