0131bc0513e1857de29a6784f1cd157d264cea47
fredericlugbull <f.lugbull@gmail.com>
2021-06-14 16:09:41 +0200

    Trying to obtain the right intel in the Objects

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,21 +9,28 @@ import data from 'src/data/data.json';
 import './styles.scss';
 
 // == Composant
-const App = () => (
+const App = () => {
+  // const parseCodeString = (code) => {{console.log("yo");}}
+  /* return object */
+
+  const dataParsed = data.map(eachString => { return {name: eachString.slice(2)}});
+  console.log("my parsed Data is:", dataParsed);
+
+  return (
   <div className="app">
-    <h1>Liste des produits</h1>
-      {/* Display of every product cards */}
-     
+  <h1>Liste des produits</h1>
+    {/* Display of every product cards */}
+   
 {/* I tried Object.keys(data).map here, and it renders 0, 1, 2, etc. And if I do Object.values(data).map, it renders the GVC lines one by one (one GVC ref by list item). Si je comprends bien l'exo, il faudrait que j'ajoute des guillemes avant et après les underscore, que je supprime les underscore, et qu'ensuite je transforme les chiffres stringés en numbers. */}
-    {data.map((productCard, i) => {
-      return (
-       <li key={i}>
-        <span>{productCard}</span>
-      </li>
-        
-      )})}
-  </div>
+  {dataParsed.map((productCard, i) => {
+    return (
+     <li key={i}>
+      <span>{productCard}</span>
+    </li>
+   )})}
+</div>
 );
+};
 
 // == Export
 export default App;
